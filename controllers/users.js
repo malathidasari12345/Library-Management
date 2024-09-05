21b705f6a1eec4913f2e7ebc29687ff9b314e3dd
malathidasari12345 <155967677+malathidasari12345@users.noreply.github.com>
2024-09-05 20:28:47 +0530

    users.js

--- controllers/users.js
+++ controllers/users.js
@@ -65,7 +65,7 @@ const login = async (req,res)=>{
     // comparing password...
     const isMatch = await bcrypt.compare(password, user.password)
     if (!isMatch) {
-        return res.status(409).json({
+        return res.status(401).json({
             success: false,
             message: "Invalid Email or Password"
         });
